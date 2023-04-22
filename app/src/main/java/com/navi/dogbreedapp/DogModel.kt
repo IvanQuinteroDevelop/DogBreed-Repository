c2a66577a35d65b3e0ad128ee64bf99625374314
IvanQuinteroDevelop <ivanquinterodevelop@gmail.com>
2023-04-22 13:36:20 -0500

    Creating DogModel.kt

---
+++ app/src/main/java/com/navi/dogbreedapp/DogModel.kt
@@ -0,0 +1,15 @@
+package com.navi.dogbreedapp
+
+data class DogModel(
+    val id: Long,
+    val index: Int,
+    val name: String,
+    val type: String,
+    val heightFemale: Double,
+    val heightMale: Double,
+    val imageUrl: String,
+    val lifeExpectancy: String,
+    val temperament: String,
+    val weightFemale: Double,
+    val weightMale: Double
+)
