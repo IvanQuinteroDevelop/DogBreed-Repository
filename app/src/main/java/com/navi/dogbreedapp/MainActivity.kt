7c781374c0cb259e155a9a9400b154c68a640383
IvanQuinteroDevelop <ivanquinterodevelop@gmail.com>
2023-04-27 09:55:03 -0500

    preparing MainActivity.kt

--- app/src/main/java/com/navi/dogbreedapp/MainActivity.kt
+++ app/src/main/java/com/navi/dogbreedapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.navi.dogbreedapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.navi.dogbreedapp.databinding.ActivityMainBinding
+import com.navi.dogbreedapp.doglist.DogListActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        manageClicks(binding)
+    }
+
+    private fun manageClicks(binding: ActivityMainBinding) {
+        binding.apply {
+            dogListButton.setOnClickListener {
+                goToActivity(DogListActivity())
+            }
+        }
+    }
+
+    private fun goToActivity(activity: Activity) {
+        val intent = Intent(this, activity::class.java)
+        startActivity(intent)
     }
 }
